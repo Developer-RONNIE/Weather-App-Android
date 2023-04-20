5721a9ef7565b4ef1557d51015c028231ec03fb3
Satvinder Singh <126100504+Satvinder9955@users.noreply.github.com>
2023-04-21 04:03:45 +0530

    Updated the CityFinder.java

--- app/src/main/java/com/example/weatherapp/cityFinder.java
+++ app/src/main/java/com/example/weatherapp/cityFinder.java
@@ -25,5 +25,21 @@ public class cityFinder extends AppCompatActivity {
                 finish();
             }
         });
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                String newCity= editText.getText().toString();
+                Intent intent=new Intent(cityFinder.this,MainActivity.class);
+                intent.putExtra("City",newCity);
+                startActivity(intent);
+
+
+
+                return false;
+            }
+        });
+
+
+
     }
 }

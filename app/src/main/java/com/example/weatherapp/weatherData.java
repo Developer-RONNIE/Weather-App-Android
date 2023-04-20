f84a95b8ffb343ea649289964f1fe47c8b672c74
RONNIE <ronnieworkspace@gmail.com>
2023-04-21 01:49:34 +0530

    committed weather data and other main activity variables

---
+++ app/src/main/java/com/example/weatherapp/weatherData.java
@@ -0,0 +1,17 @@
+package com.example.weatherapp;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class weatherData {
+
+    private String mTemperature,micon,mcity,mWeatherType;
+    private int mCondition;
+
+    public static weatherData fromJson(JSONObject jsonObject)
+    {
+
+    }
+
+
+}

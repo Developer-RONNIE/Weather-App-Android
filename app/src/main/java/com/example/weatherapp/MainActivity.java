2f211849db73ac80da53199dbbb2ab83d35a2c8c
Satvinder Singh <126100504+Satvinder9955@users.noreply.github.com>
2023-04-20 21:14:55 +0530

    editted java file

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -3,6 +3,26 @@ package com.example.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import org.json.JSONObject;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +30,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // weatherState = findViewById(R.id.weatherCondition);
+        //Temperature = findViewById(R.id.temperature);
+        //mweatherIcon = findViewById(R.id.weatherIcon);
+        //mCityFinder = findViewById(R.id.cityFinder);
+        //NameofCity = findViewById(R.id.cityName);
     }
 }

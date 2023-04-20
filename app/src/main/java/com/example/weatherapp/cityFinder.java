e4a99d75052cdbd722244064fbe5fa5fad896aec
Satvinder Singh <126100504+Satvinder9955@users.noreply.github.com>
2023-04-21 02:57:10 +0530

    Updated the CityFinder.java

---
+++ app/src/main/java/com/example/weatherapp/cityFinder.java
@@ -0,0 +1,29 @@
+package com.example.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+public class cityFinder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_finder);
+        final EditText editText = findViewById(R.id.searchCity);
+        ImageView backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
